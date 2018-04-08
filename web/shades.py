2005821ceee8a23903d5fe769ae79210f9076ff3
tyu2 <tyu2@swarthmore.edu>
2018-04-08 15:21:44 -0400

    asdf

--- web/shades.py
+++ web/shades.py
@@ -12,13 +12,13 @@ class Shades(object):
     @cherrypy.expose
     def index(self):
         return text.format("")
-    
+
     @cherrypy.expose
     def up(self):
-        command = "python2 -c 'import control_shades; control_shades.move_shades('up',0,50)'"
-        
-        return ''.join(random.sample(string.hexdigits, int(length)))
-    
+        command = "python2 -c 'import control_shades; control_shades.move_up()"
+        result = subprocess.Popen(command)
+        return
+
 if __name__=='__main__':
     cherrypy.config.update({'server.socket_host': '0.0.0.0', 'server.socket_port': 8181})
     cherrypy.quickstart(StringGenerator())
