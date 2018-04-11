6b31291068a67bda6d3cf60023186c833f89e898
tyu2 <tyu2@swarthmore.edu>
2018-04-10 21:44:27 -0400

    asdf

--- our_code/webControl.py
+++ our_code/webControl.py
@@ -43,7 +43,7 @@ class Shades(object):
 
     @cherrypy.expose
     def allDown(self):
-        var = ["0", str(Shades.current), str(Shades.max)]
+        var = [str(Shades.current), str(Shades.max)]
         command = "python2 -c 'import control_shades; control_shades.allDown("+", ".join(var)+")'"
         result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
         Shades.current = Shades.max
@@ -51,7 +51,7 @@ class Shades(object):
 
     @cherrypy.expose
     def allUp(self):
-        var = ["1", str(Shades.current), "0"]
+        var = [str(Shades.current), "0"]
         command = "python2 -c 'import control_shades; control_shades.allUp("+", ".join(var)+")'"
         result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
         Shades.current = 0
