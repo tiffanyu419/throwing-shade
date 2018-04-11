70fd0d0e4a97d3a4e2fcfe069556d92456c9c6c5
tyu2 <tyu2@swarthmore.edu>
2018-04-10 21:31:56 -0400

    asdf

--- our_code/webControl.py
+++ our_code/webControl.py
@@ -7,32 +7,56 @@ control_text = """
 </form>
 <form method="get" action="down">
 <button type="submit">down</button>
-</form></body>
+</form>
+<form method="get" action="allUp">
+<button type="submit">allUp</button>
+</form>
+<form method="get" action="allDown">
+<button type="submit">allDown</button>
+</form>
+</body>
 </html>
 """
 
 class Shades(object):
-    count = 0
+    current = 0
+    max = 80
     @cherrypy.expose
     def index(self):
         return control_text.format("")
 
     @cherrypy.expose
     def up(self):
-        command = "python2 -c 'import control_shades; control_shades.move_up(" +str(Shades.count)+")'"
+        command = "python2 -c 'import control_shades; control_shades.move_up(" +str(Shades.curret)+")'"
         result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
         ls = result.split()
-        Shades.count = int(ls[4])
-        return str(Shades.count)
+        Shades.current = int(ls[4])
+        return str(Shades.current)
 
     @cherrypy.expose
     def down(self):
-        command = "python2 -c 'import control_shades; control_shades.move_down("+str(Shades.count)+")'"
+        command = "python2 -c 'import control_shades; control_shades.move_down("+str(Shades.current)+")'"
         result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
         ls = result.split()
-        Shades.count = int(ls[4])
-        return str(Shades.count)
+        Shades.current = int(ls[4])
+        return str(Shades.current)
 
+    @cherrypy.expose
+    def allDown(self):
+        var = ["down", str(Shades.current), str(Shades.max)]
+        command = "python2 -c 'import control_shades; control_shades.move_shades("+", ".join(var)+")'"
+        result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
+        Shades.current = Shades.max
+        return str(Shades.current)
+
+    @cherrypy.expose
+    def allDown(self):
+        var = ["up", str(Shades.current), "0"]
+        command = "python2 -c 'import control_shades; control_shades.move_shades("+", ".join(var)+")'"
+        result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
+        Shades.current = 0
+        return str(Shades.current)
+        
 if __name__=='__main__':
     cherrypy.config.update({'server.socket_host': '0.0.0.0', 'server.socket_port': 8181})
     cherrypy.quickstart(Shades())
