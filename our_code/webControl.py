3b17ab9f490fac6c1ab620cf6ba3cc12f4df0829
tyu2 <tyu2@swarthmore.edu>
2018-04-10 21:13:10 -0400

    asdf

--- our_code/webControl.py
+++ our_code/webControl.py
@@ -23,7 +23,6 @@ class Shades(object):
         command = "python2 -c 'import control_shades; control_shades.move_up(" +str(Shades.count)+")'"
         result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8').replace('\n', '<br>')
         ls = result.splitlines()
-
         return str(len(ls))
 
     @cherrypy.expose
