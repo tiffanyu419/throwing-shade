5140609136de1d5bdecf288a28047c28bd459516
tyu2 <tyu2@swarthmore.edu>
2018-04-10 20:51:01 -0400

    asdf

--- our_code/webControl.py
+++ our_code/webControl.py
@@ -22,7 +22,7 @@ class Shades(object):
         #command = "python2 -c 'import control_shades; control_shades.move_up()'"
         command = "python2 -c 'import control_shades; control_shades.move_up(" +str(Shades.count)+")'"
         result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8').replace('\n', '<br>')
-        return "Current state:" + result[1]
+        return "Current state:" + result
 
     @cherrypy.expose
     def down(self):
