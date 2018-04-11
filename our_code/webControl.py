de1da95f6facf6cc7b994bda9b0eb88837292da9
tyu2 <tyu2@swarthmore.edu>
2018-04-10 20:15:49 -0400

    asdf

--- our_code/webControl.py
+++ our_code/webControl.py
@@ -22,14 +22,14 @@ class Shades(object):
         #command = "python2 -c 'import control_shades; control_shades.move_up()'"
         command = "python2 -c 'import control_shades; control_shades.move_up(40)'"
         Shades.count = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-        return str(Shades.count)
+        return "Current state:" + str(Shades.count)
 
     @cherrypy.expose
     def down(self):
         #command = "python2 -c 'import control_shades; control_shades.move_down()'"
         command = "python2 -c 'import control_shades; control_shades.move_down(40)'"
         Shades.count = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-        return str(Shades.count)
+        return "Current state:" + str(Shades.count)
 
 if __name__=='__main__':
     cherrypy.config.update({'server.socket_host': '0.0.0.0', 'server.socket_port': 8181})
