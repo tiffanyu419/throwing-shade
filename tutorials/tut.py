7d2e24fce29f21f77969449820a1665cce295519
tyu2 <tyu2@swarthmore.edu>
2018-04-15 16:42:39 -0400

    asdf

--- tutorials/tut.py
+++ tutorials/tut.py
@@ -10,7 +10,7 @@ class StringGenerator(object):
     def index(self):
         return """<html>
           <head>
-            <link href="./Public/css/style.css" rel="stylesheet">
+            <link href="home/pi/Public/css/style.css" rel="stylesheet">
           </head>
           <body>
             <form method="get" action="generate">
