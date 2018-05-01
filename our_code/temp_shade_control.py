92f2b6c4baa6ca2e2dbfa349d1729d03c5e38fae
tyu2 <tyu2@swarthmore.edu>
2018-05-01 10:06:16 -0400

    asdf

--- our_code/temp_shade_control.py
+++ our_code/temp_shade_control.py
@@ -9,9 +9,9 @@ def main():
     csv = open(download_dir, "w")
     columnTitleRow = "Date, Time, amb-sat, amb-des, state, amb, sat, lock"
     csv.write(columnTitleRow)
-    
+
     locked = 0
-    # state 0 for close, 1 for open 
+    # state 0 for close, 1 for open
     state = 0
     new_state = 0
     shade_state = 2500
@@ -23,7 +23,7 @@ def main():
     sat_amb_diff = 0
     # desired temperature
     des_temp = 22
-    
+
     while (1):
         now = datetime.datetime.now()
         if now.hour < 6:
@@ -32,15 +32,15 @@ def main():
             locked = 1
         else:
             locked = 0
-            
+
         print("Collecting data")
         #collect temp and find the difference every minute for 30 minutes
         for i in range(3):
-            time.sleep(3)
+            time.sleep(60)
             amb = read_data.read_temp_a()[0]
             sat = read_data.read_temp_b()[0]
             sat_amb_diff += (amb-sat)
-            
+
         print ("total: %f" %(sat_amb_diff))
         #calculate the difference between the desired and the ambient temperature
         des_amb_diff = amb - des_temp #negative want it heat up, pos cool down
@@ -55,19 +55,19 @@ def main():
             new_state = 0
         elif des_amb_diff < -1 and sat_amb_diff > 1:
             new_state = 1
-        
+
         print("lock: %d new_state: %d state: %d" %(locked, new_state, state))
         if locked == 0 and new_state != state:
             #move shade
             print("Move shade")
             if new_state == 1:
-                control_shade.allUp(shade_state, 0)
+                control_shades.allUp(shade_state, 0)
                 shade_state = 0
-                
+
             else:
                 control_shades.allDown(shade_state, max_length)
                 shade_state = max_length
-                
+
 	now = datetime.datetime.now()
 	cur_time = str(now.hour)+":"+str(now.minute)
 	date = str(now.month) + "/" + str(now.day)
