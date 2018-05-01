2de4d0858ba12429fc1dbc5f43f95786bb9fb14e
tyu2 <tyu2@swarthmore.edu>
2018-05-01 10:50:30 -0400

    asdf

--- our_code/webControl.py
+++ our_code/webControl.py
@@ -3,8 +3,8 @@ import cherrypy
 import subprocess
 
 class Shades(object):
-    current = 2500
-    max = 2500
+    current = 2400
+    max = 2400
     @cherrypy.expose
     def index(self):
         return open('index.html')
